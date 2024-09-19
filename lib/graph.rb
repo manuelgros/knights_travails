e0560290224c32bfed993bbfeab8c02b86d741a0
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-09-19 14:05:22 -0700

    Created Graph class with methods to populate Graph with nodes and edges

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,1 +1,30 @@
 # Graph class
+require './lib/node'
+require './lib/creatable_chessboard'
+
+class Graph
+  include CreatableChessBoard
+
+  attr_accessor :nodes
+
+  def initialize
+    @nodes = {}  
+  end
+
+  def add_node(node)
+    @nodes[node.coordinates] = node
+  end
+
+  def add_edge(node1, node2)
+    @nodes[node1].add_edge(@nodes[node2])
+    # @nodes[node2].add_edge(@nodes[node1])
+  end
+
+  #  doesn't work as intended
+  def print_graph
+    @nodes.each do |position, node|
+      puts "#{position} : #{node.print_moves.select {|node| node.coordinates}}"
+    end
+    return nil
+  end
+end
