c4cc3b155228c0c388cd7efe2c1f3f3e3102af1c
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-09-19 14:05:47 -0700

    Created Node class with methods to create Nodes and establish edges

--- lib/node.rb
+++ lib/node.rb
@@ -1,1 +1,22 @@
 # Node class
+
+class Node
+  attr_accessor :coordinates, :possible_moves
+
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @possible_moves = []
+  end
+
+  def add_edge(node)
+    @possible_moves << node
+  end
+
+  #  doesn't work as intended
+  def print_moves
+    @possible_moves.each do |neighbour_node|
+      print "#{neighbour_node.coordinates} "
+    end
+    return nil
+  end
+end
