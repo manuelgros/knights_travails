788cb97c7db5df02fe659d5db411772296f4aa42
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-09-24 13:48:35 -0700

    Added test code

--- main.rb
+++ main.rb
@@ -2,21 +2,7 @@
 require './lib/node'
 require './lib/graph'
 
-# Creating the "chess board"
-# def create_chess_board(new_graph)
-#   arr = Array (0..7)
-#   combination_arr = arr.reduce([]) do |pair_arr, first_ele|
-#     arr.each { |sec_ele| pair_arr << [first_ele, sec_ele] }
-#     pair_arr
-#   end
-#   combination_arr.each do |coordinate|
-#     new_graph.add_node(Node.new(coordinate))
-#   end
-# end
-
-# my_graph = Graph.new
-# my_graph.create_chess_board
-# p my_graph
-
-# my_graph.nodes.each_key { |node_key| my_graph.create_all_pathways(node_key) }
-# p my_graph
+graph = Graph.new
+graph.create_chess_board
+graph.connect_all_nodes
+graph.knight_moves([3,3], [7,5])
