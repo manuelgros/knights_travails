a4b3070d481c22bb76f311854edf819f953a0f94
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-09-24 14:20:33 -0700

    Cleaned up and refactored code

--- lib/creatable_chessboard.rb
+++ lib/creatable_chessboard.rb
@@ -28,53 +28,29 @@ module CreatableChessBoard
     # print "END" #for debugging
   end
 
-  # def bfs(start, par, dist)
-  #   queue = []
-  #   dist[start] = 0
-  #   queue.push(start)
-
-  #   until queue.empty?
-  #     active_node = queue.shift
-
-  #     for neighbor in self.nodes[active_node].possible_moves
-  #       if dist[neighbor.coordinates] == Float::INFINITY
-  #         par[neighbor.coordinates] = active_node
-  #         dist[neighbor.coordinates] = dist[active_node] + 1
-  #         queue.push(neighbor.coordinates)
-  #       end
-  #     end
-  #   end
-  # end
-
   def knight_moves(start, destination)
     return 'Destination is outside the board' unless self.nodes.keys.include?(destination)
 
     queue = [start]
-    par = {}
-    dist = {}
-    self.nodes.keys.each do |node_coordinate| 
-      dist[node_coordinate] = Float::INFINITY
-      par[node_coordinate] = nil
-    end
-    dist[start] = 0
+    visited = [start]
+    par = Hash[self.nodes.keys.map {|coordinate| [coordinate, nil]}]
 
     until queue.empty? do
       active_node = queue.shift
       self.nodes[active_node].possible_moves.each do |neighbor|
-        if dist[neighbor.coordinates] == Float::INFINITY
+        unless visited.include?(neighbor.coordinates)
           par[neighbor.coordinates] = active_node
-          dist[neighbor.coordinates] = dist[active_node] + 1
+          visited.push(neighbor.coordinates)
           queue.push(neighbor.coordinates)
         end
       end
     end
 
-    path = []
-    current_node = destination
-    path.push(destination)
-    while par[current_node] != nil
-      path.push(par[current_node])
-      current_node = par[current_node]
+    path = [destination]
+    current_position = destination
+    while par[current_position] != nil
+      path.push(par[current_position])
+      current_position = par[current_position]
     end
     puts "The knight can reach #{destination} in #{path.size-1} moves."
     puts "The path is:"
